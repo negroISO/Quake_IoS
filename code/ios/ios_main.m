671588a19e4b146e7832b5c02fe8f1bc56b3fce6
NegroISO <negroISO@users.noreply.github.com>
2026-04-13 20:48:30 -0500

    Force com_maxfps to 120 on iOS startup so ProMotion isn't limited by archived engine config

--- code/ios/ios_main.m
+++ code/ios/ios_main.m
@@ -396,6 +396,7 @@ void Quake3_Init(const char *basePath) {
 
     char cmdline[256] = "";
     Com_Init(cmdline);
+    Cbuf_AddText("com_maxfps 120\n");
     Cbuf_AddText("map q3dm1\n");
     engine_initialized = qtrue;
 
